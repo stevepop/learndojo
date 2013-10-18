a7851d8bd20974f496dfe77b6fe0a7d661b67c6c
Steve Popoola <stevepop@gmail.com>
2013-10-18 05:19:53 -0400

    Can now load local modules when using CDN

---
+++ js/app/Hello.js
@@ -0,0 +1,6 @@
+define([ "dojo/_base/declare", "dijit/Dialog" ], function(declare, Dialog){
+	// declare our custom class
+	return dojo.declare(dijit.Dialog, {
+		title: "Hi"
+	});
+});
--- js/mydojo.js
+++ js/mydojo.js
@@ -1,5 +1,14 @@
-require(["dojo/dom", "dojo/domReady!"], 
-    function(dom){
+require(["app/Hello","dojo/dom", "dojo/domReady!"], 
+    function(Hello, dom){
 		dom.byId("helloworld").innerHTML = "Hello New World!";
+			var w = new Hello({
+					id: "hello",
+					content: "Hello World!"
+
+				// place it in the DOM
+				}).placeAt(document.body);
+
+				// show it
+				w.show();
     }
 );
